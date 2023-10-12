72ac5e10406075b78296aaca99d270fc99d8154a
Kohei Okazaki <kohei.a.okazaki@dena.com>
2023-10-13 08:41:24 +0900

    change

--- application/bittorrent_app.py
+++ application/bittorrent_app.py
@@ -1,4 +1,19 @@
+from .bittorrent import BitTorrent
+
+
+class AlreadyExist(Exception):
+    pass
+
+
 class BitTorrentApp:
     def __init__(self):
-        pass
+        self.file_path = ""
+        self.bittorrent_threads = {}
 
+    def register(self, torrent):
+        if torrent.info_hash in self.bittorrent_threads:
+            raise AlreadyExist('This BitTorrentContent is already registered.')
+
+        bittorrent_thread = BitTorrent(torrent, self.file_path)
+        self.bittorrent_threads[torrent.info_hash] = bittorrent_thread
+        bittorrent_thread.run()
