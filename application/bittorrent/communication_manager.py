0535acac2fde26c3aa90bdd3b454c9fa544390a9
Okazaki <net.kohei.okazaki@gmail.com>
2023-10-24 01:21:04 +0900

    change

--- application/bittorrent/communication_manager.py
+++ application/bittorrent/communication_manager.py
@@ -44,7 +44,7 @@ class CommunicationManager:
         # await self.remove_unhealthy_peer()
 
         # イテレート中の変更時にエラーを回避するためにcopy()
-        for peer in self.peers.copy():
+        for peer in self.peers:
             logger.debug(f"ip: {peer.ip}")
             try:
                 payload = await peer.reader.read(4096)
